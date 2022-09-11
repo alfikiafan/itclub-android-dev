126fcadff05aef1ead351505b0c70cbbe58c696e
Alfiki Diastama Afan Firdaus <89908618+alfikiafan@users.noreply.github.com>
2022-09-11 19:59:11 +0700

    Create HelloWorld.dart

--- 1 - Dasar-Dasar Flutter/HelloWorld.dart
+++ 1 - Dasar-Dasar Flutter/HelloWorld.dart
@@ -1,25 +1,27 @@
 import 'package:flutter/material.dart';
- 
+
 void main() {
- runApp(MyApp());
+  runApp(MyApp());
 }
- 
+
 class MyApp extends StatelessWidget {
- @override
- Widget build(BuildContext context) {
-   return MaterialApp(
-     title: 'IT CLUB Android Dev Demo',
-     theme: ThemeData(
-       primarySwatch: Colors.blue,
-     ),
-     home: Scaffold(
-       appBar: AppBar(
-         title: Text('Ini kode pertamaku!'),
-       ),
-       body: Center(
-         child: Text('Semangat temen-temen!'),
-       ),
-     ),
-   );
- }
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'IT CLUB Android Dev Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Ini kode pertamaku!'),
+        ),
+        body: Center(
+          child: Text('Semangat temen-temen!'),
+        ),
+      ),
+    );
+  }
 }
