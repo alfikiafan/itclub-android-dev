8c1f924c1afa598e68f854a19780b63af4bb9d72
Alfiki Diastama Afan Firdaus <89908618+alfikiafan@users.noreply.github.com>
2022-10-08 13:32:11 +0700

    Update and rename Source Code/ProjectTahap1.dart to Source Code/Project Tahap 1/main.dart

--- Source Code/Project Tahap 1/main.dart
+++ Source Code/Project Tahap 1/main.dart
@@ -1,23 +1,23 @@
 import 'package:flutter/material.dart';
- 
+
 void main() => runApp(const MyApp());
- 
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
- 
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Wisata Solo',
+      title: 'Wisata Bandung',
       theme: ThemeData(),
       home: const DetailScreen(),
     );
   }
 }
- 
+
 class DetailScreen extends StatelessWidget {
   const DetailScreen({Key? key}) : super(key: key);
- 
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -45,7 +45,7 @@ class DetailScreen extends StatelessWidget {
                     children: const <Widget>[
                       Icon(Icons.calendar_today),
                       SizedBox(height: 8.0),
-                      Text('Open Saturday - Thursday'),
+                      Text('Open Saturday-Thursday'),
                     ],
                   ),
                   Column(
